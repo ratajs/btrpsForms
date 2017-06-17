79c4d482588ceab613982c15a7380554c6cd0174
ratajs <ratajs@users.noreply.github.com>
2017-06-17 17:53:04 +0200

    Create formsAlt.php

---
+++ formsAlt.php
@@ -0,0 +1,97 @@
+<?php
+  class BtrpsForm
+  {
+    private $fields = [];
+    private $submitText = "Send";
+    public function __construct($sText = "Send") {
+      $this->submitText = $sText;
+    }
+    public function addField($name, $label, $type = "text", $val = NULL, $placeholder = NULL, $required = false)
+    {
+      $this->fields[] = (object) ['name' => $name, 'label' => $label, 'type' => $type, 'val' => $val, 'placeholder' => $placeholder, 'required' => $required];
+      return true;
+    }
+    public function __toString()
+    {
+      $r = '<form method="POST" role="form">';
+      foreach($this->fields as $k => $v)
+      {
+        $n = $v->name;
+        $l = $v->label;
+        $t = $v->type;  
+        $val = $v->val;
+        $p = $v->placeholder;
+        $req = $v->required;
+        $r.= '
+          <div class="form-group row">
+            <label for="input' . $k . '" class="control-label col-2"' . (in_array($t, ["radio", "select"]) ? NULL : ' style="cursor: pointer"') . '>' . $l . '</label>';
+        if(in_array($t, ["radio", "select"]))
+        {
+          if(!is_array($val))
+            die("Error: Values in radio or select control must be array!");
+          if($t=="radio")
+          {
+            $isFirst = true;
+            foreach($val as $k2 => $v2)
+            {
+              if($isFirst)
+              {
+                $r.= '
+                  <div class="radio col-10"><label><input type="radio" name="' . $n . '" value="' . $k2 . '" checked> ' . $v2 . '</label></div><div>';
+                $isFirst = false;
+              } else
+              {
+                $r.= '
+                  <div class="radio offset-2 col-10"><label><input type="radio" name="' . $n . '" value="' . $k2 . '"> ' . $v2 . '</label></div></div>';
+                if($k==count((array) $this->fields)-1)
+                {
+                   $r.= '
+                <div class="form-group row">
+                  <div class="col-xs-12 offset-2 col-10">
+                    <input type="submit" value="' . $this->submitText . '" class="btn btn-secondary">
+                  </div>
+                </div>';
+                };
+              };
+            };
+          } else
+          {
+            $r.= '<select name="' . $n . '" class="form-control">';
+            foreach($val as $k2 => $v2)
+            {
+              $r.= '
+                <option value="' . $k2 . '">' . $v2 . '</option>
+              ';
+            };
+            $r.= "</select></div>";
+            if($k==count((array) $this->fields)-1)
+            {
+              $r.= '</div>
+                <div class="form-group row">
+                  <div class="col-xs-12 offset-2 col-10">
+                    <input type="submit" value="' . $this->submitText . '" class="btn btn-secondary">
+                  </div>
+                </div>';
+            }
+          };
+        } else
+        {
+          $r.= '
+            <input type="' . $t . '" name="' . $n . '" value="' . $val . '" placeholder="' . $p . '" class="form-control col-10" id="input' . $k . '"' . ($k==0 ? ' autofocus' : NULL) . ($req ? ' required' : NULL) . '>
+          </div>
+            ';
+        };
+        if($k==count((array) $this->fields)-1)
+        {
+          $r.= '
+          <div class="form-group row">
+            <div class="col-xs-12 offset-2 col-10">
+              <input type="submit" value="' . $this->submitText . '" class="btn btn-secondary">
+            </div>
+          </div>';
+        }
+      };
+      return $r . "\n</form>\n";
+    }
+  };
+?>
